53d207256d7dd3bbfddb3606491281bfd88f29db
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-29 22:05:43 +0100

    stampa numeri da 1 a 100 sulla console

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// 1 - stampa i numeri da 1 a 100 sulla console
+
+for (let i = 1; i <= 100; i++) {
+	let number = i;
+	console.log(number);
+}
+
+// 2 - sostituisci i numeri con le fizz o buzz a seconda del multiplo
+
+// 2.1 - sostituisci con "Fizz" i multipli di 3
+
+
+
+// 2.2 - sostituisci con "Buzz" i multipli di 5
+
+
+
+// 2.3 - sostituisci con "FizzBuzz" i multipli di 3 e 5
+
+
