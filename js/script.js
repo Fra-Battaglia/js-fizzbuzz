8aeac0504f369b62a42fbcaedcf04dbee1ab666f
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-29 23:21:56 +0100

    sostituiti con FizzBuzz i multipli di 3 e 5

--- js/script.js
+++ js/script.js
@@ -6,19 +6,24 @@ for (let i = 1; i <= 100; i++) {
 	
 	// 2 - sostituisci i numeri con le fizz o buzz a seconda del multiplo
 	
-	// 2.1 - sostituisci con "Fizz" i multipli di 3
+	// 2.1 - sostituisci con "FizzBuzz" i multipli di 3 e 5
+
+	if (number%3 == 0 && number%5 == 0) {
+		number = `FizzBuzz`;
+	}
+
+	// 2.2 - sostituisci con "Fizz" i multipli di 3
 	
-	if (number%3 == 0) {
+	else if (number%3 == 0) {
 		number = `Fizz`;
 	}
 	
-	// 2.2 - sostituisci con "Buzz" i multipli di 5
+	// 2.3 - sostituisci con "Buzz" i multipli di 5
 	
 	else if (number%5 == 0) {
 		number = `Buzz`;
 	}
 	
-	// 2.3 - sostituisci con "FizzBuzz" i multipli di 3 e 5
 	console.log(number);
 }
 
