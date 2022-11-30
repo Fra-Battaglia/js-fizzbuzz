fc6e5a36499a7abe04600d75f4689ba812b113d9
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-30 13:02:41 +0100

    exercise done

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
-// 1 - stampa i numeri da 1 a 100 sulla console
+// 1 - dichiara i numeri da 1 a 100
+
+const container = document.querySelector('.container');
 
 for (let i = 1; i <= 100; i++) {
-	let number = i;
-	
 	
 	// 2 - sostituisci i numeri con le fizz o buzz a seconda del multiplo
 	
 	// 2.1 - sostituisci con "FizzBuzz" i multipli di 3 e 5
 
-	if (number%3 == 0 && number%5 == 0) {
-		number = `FizzBuzz`;
+	if (i % 3 == 0 && i % 5 == 0) {
+		let number = `FizzBuzz`;
+		container.innerHTML += `<div class="number fizzbuzz">${number}</div>`;
 	}
 
 	// 2.2 - sostituisci con "Fizz" i multipli di 3
 	
-	else if (number%3 == 0) {
-		number = `Fizz`;
+	else if (i % 3 == 0) {
+		let number = `Fizz`;
+		container.innerHTML += `<div class="number fizz">${number}</div>`;
 	}
 	
 	// 2.3 - sostituisci con "Buzz" i multipli di 5
 	
-	else if (number%5 == 0) {
-		number = `Buzz`;
+	else if (i % 5 == 0) {
+		let number = `Buzz`;
+		container.innerHTML += `<div class="number buzz">${number}</div>`;
+	}
+
+	else {
+		let number = i;
+		container.innerHTML += `<div class="number">${number}</div>`;
 	}
 	
-	console.log(number);
+	// 3 - Stampa tutto
+
+	console.log(i);
 }
 
 
